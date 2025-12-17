9182c29fd7d390c1c5dfa7f390e1e402b06cfb6c
Vrushti Jagda <vrushtijagda@gmail.com>
2025-12-17 19:26:19 +0530

    case sensitive

--- frontend/src/ViewEmployee.jsx
+++ frontend/src/ViewEmployee.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { toast } from "react-toastify";
-import "./viewEmployee.css";
+import "./ViewEmployee.css";
 
 const ViewEmployee = () => {
   const { employee_id } = useParams();
